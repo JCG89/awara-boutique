af396131d877af2fc550f7ef38a794945252c367
Jean Christophe Gomis <devdujour@yahoo.com>
2021-05-19 12:17:31 +0200

    modification du fichier.env, d'InscriptionController, templates/gomis/index.html.twig, templates/order/add.html.twig et upload d'image dans le back-office

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -16,7 +16,8 @@ class InscriptionController extends AbstractController
 {
     private $em;
 
-    public function __construct(EntityManagerInterface $em){
+    public function __construct(EntityManagerInterface $em)
+    {
         $this->em = $em;
     }
     /**
@@ -31,39 +32,39 @@ class InscriptionController extends AbstractController
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted()&& $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
 
-           $user = $form->getData();
-        //    $search_email= $this->em->getRepository( User::class)->findOneByEmail($user->getEmail());
+            $user = $form->getData();
+            //    $search_email= $this->em->getRepository( User::class)->findOneByEmail($user->getEmail());
 
-        //    if(!$search_email){
-            $password = $encoder->encodePassword($user,$user->getPassword());
+            //    if(!$search_email){
+            $password = $encoder->encodePassword($user, $user->getPassword());
             $user->setPassword($password);
-           
+
             $this->em->persist($user);
             $this->em->flush();
 
             // $mail = new Mail();
             // $content = 'Bonjour'.'$user->getPrenom().'</br>Bienvenu dans KinkiHair'</br>
             // $mail->send(user->getEmail),user->getPrenom(), 'Bienvenue dans Kinkihair'
- 
+
             $notification = 'Votre inscription a bien été pris en compte vous pouvez dés à présent vous connecter à votre compte';
-        //   }
-                      
-        //    else{
-          
-        //     $notification = 'L\'email que vous avez renseigné existe déjà !';
-        // }
-           
-             return $this->redirectToRoute('gomis');     
+            //   }
+
+            //    else{
+
+            //     $notification = 'L\'email que vous avez renseigné existe déjà !';
+            // }
+
+            return $this->redirectToRoute('gomis');
         }
-        
-        
-        return $this->render('inscription/index.html.twig',[
 
-            'form'=>$form->createView(),
 
-            'notification'=>$notification
+        return $this->render('inscription/index.html.twig', [
+
+            'form' => $form->createView(),
+
+            'notification' => $notification
 
         ]);
     }
